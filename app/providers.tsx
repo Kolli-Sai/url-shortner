07a98eafb8fd0a0c9f86c313236437b26c5c0688
KolliSai <saik98187@gmail.com>
2023-10-03 22:22:16 +0530

    changed navbar to client compo

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,13 +3,16 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
-        {children}
-      </NextThemesProvider>
+      <SessionProvider>
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+          {children}
+        </NextThemesProvider>
+      </SessionProvider>
     </NextUIProvider>
   );
 }
